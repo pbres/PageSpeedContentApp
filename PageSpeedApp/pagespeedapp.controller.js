453bd7f480dcc0b83e95378cdccd5c79bc6a3b95
Pawel Bres <pawel@polarbits.co>
2018-10-20 15:37:47 +0200

    Initial commit

---
+++ PageSpeedApp/pagespeedapp.controller.js
@@ -0,0 +1,28 @@
+﻿angular.module("umbraco")
+    .controller("ContentApps.PageSpeedApp", function ($scope, $http, $location, $window, editorState) {
+
+        var vm = this;
+        vm.CurrentNodeId = editorState.current.id;
+        
+        if (vm.CurrentNodeId !== 0) {
+            vm.statusText = "Loading...";
+            vm.title = editorState.current.variants[0].name;
+            vm.completed = false;
+
+            var baseUrl = new $window.URL($location.absUrl()).origin;
+            var nodeUrl = baseUrl + editorState.current.urls[0].text;
+            
+            $http({
+                method: 'GET',
+                url: 'https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=' + nodeUrl
+            }).then(function successCallback(response) {
+                vm.score = response.data.ruleGroups.SPEED.score;
+                vm.rules = response.data.formattedResults.ruleResults;
+                vm.statusText = '';
+                vm.completed = true;
+            }, function errorCallback(response) {
+                console.log(response);
+                vm.statusText = 'Unable to get PageSpeed Insights for this page';
+            });
+        }
+    });
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# PageSpeedContentApp
+# PageSpeed Content App
+
+Simple Content App for Umbraco v8 displaying PageSpeed Insights for current node
+
+## Install
+
+Clone or download this repository and copy PageSpeedApp folder to the `App_Plugins` folder.
+
+At some level of usage Google Api may require an API key to be added to the query. 
